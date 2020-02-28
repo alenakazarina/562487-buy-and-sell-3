1a2575d7a921bc0fcd7199228523f732f988b757
Alena Kazarina <alenakazarina1@gmail.com>
2020-02-28 21:44:30 +0300

    added cli commands

---
+++ src/service/cli/generate.js
@@ -0,0 +1,52 @@
+'use strict';
+const fs = require(`fs`);
+const {getRandomInteger, shuffle} = require(`../utils/utils`);
+const {ExitCode} = require(`../const`);
+const {
+  TITLES,
+  PICTURES,
+  DESCRIPTIONS,
+  DESCRIPTIONS_MAX_COUNT,
+  OfferType,
+  PriceLimit,
+  Category
+} = require(`../mock/mock`);
+
+const DEFAULT_COUNT = 1;
+const MAX_INPUT_COUNT = 1000;
+
+const generateOffers = (count) => {
+  const categories = Object.values(Category);
+  return Array(count).fill(``).map(() => ({
+    "type": Object.values(OfferType)[getRandomInteger(0, 1)],
+    "title": shuffle(TITLES)[getRandomInteger(0, TITLES.length - 1)],
+    "description": shuffle(DESCRIPTIONS).slice(0, getRandomInteger(0, DESCRIPTIONS_MAX_COUNT)).join(` `),
+    "sum": getRandomInteger(PriceLimit.MIN, PriceLimit.MAX),
+    "picture": shuffle(PICTURES)[getRandomInteger(0, PICTURES.length - 1)],
+    "category": shuffle(categories)[getRandomInteger(0, categories.length - 1)]
+  }));
+};
+
+module.exports = {
+  name: `--generate`,
+  run: (count) => {
+    const offersCount = Number.parseInt(count, 10) || DEFAULT_COUNT;
+
+    if (count > MAX_INPUT_COUNT) {
+      console.log(`Не больше 1000 объявлений`);
+      process.exit(ExitCode.ERROR);
+    }
+
+    const content = JSON.stringify(generateOffers(offersCount));
+    const mockFilePath = `./src/mocks.json`;
+
+    fs.writeFile(mockFilePath, content, (err) => {
+      if (err) {
+        console.error(`Can't write data to file...`);
+        process.exit(ExitCode.ERROR);
+      }
+      console.info(`Operation success. File created: ${mockFilePath}`);
+      process.exit(ExitCode.SUCCESS);
+    });
+  }
+};
--- src/service/service.js
+++ src/service/service.js
@@ -1,3 +1,13 @@
 'use strict';
+const {Cli} = require(`./cli`);
+const {DEFAULT_COMMAND, ExitCode} = require(`./const`);
 
-console.log(`Hello world!`);
+const [, , ...args] = process.argv;
+const [userCommand, count] = args;
+
+if (userCommand === `undefined` || !Cli[userCommand]) {
+  Cli[DEFAULT_COMMAND].run();
+  process.exit(ExitCode.success);
+}
+
+Cli[userCommand].run(count);
