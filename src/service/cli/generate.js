9795f031bf250a070442d3771e8f6dbcd4041f0f
Alena Kazarina <alenakazarina1@gmail.com>
2020-02-29 14:41:56 +0300

    fixed

--- src/service/cli/generate.js
+++ src/service/cli/generate.js
@@ -17,13 +17,13 @@ const MAX_INPUT_COUNT = 1000;
 
 const generateOffers = (count) => {
   const categories = Object.values(Category);
-  return Array(count).fill(``).map(() => ({
+  return Array.from({length: count}, () => ({
     "type": Object.values(OfferType)[getRandomInteger(0, 1)],
     "title": shuffle(TITLES)[getRandomInteger(0, TITLES.length - 1)],
     "description": shuffle(DESCRIPTIONS).slice(0, getRandomInteger(0, DESCRIPTIONS_MAX_COUNT)).join(` `),
     "sum": getRandomInteger(PriceLimit.MIN, PriceLimit.MAX),
     "picture": shuffle(PICTURES)[getRandomInteger(0, PICTURES.length - 1)],
-    "category": shuffle(categories)[getRandomInteger(0, categories.length - 1)]
+    "category": shuffle(categories).slice(0, [getRandomInteger(1, categories.length - 1)])
   }));
 };
 
@@ -33,12 +33,12 @@ module.exports = {
     const offersCount = Number.parseInt(count, 10) || DEFAULT_COUNT;
 
     if (count > MAX_INPUT_COUNT) {
-      console.log(`Не больше 1000 объявлений`);
+      console.error(`Не больше 1000 объявлений`);
       process.exit(ExitCode.ERROR);
     }
 
     const content = JSON.stringify(generateOffers(offersCount));
-    const mockFilePath = `./src/mocks.json`;
+    const mockFilePath = `./mocks.json`;
 
     fs.writeFile(mockFilePath, content, (err) => {
       if (err) {
@@ -46,7 +46,6 @@ module.exports = {
         process.exit(ExitCode.ERROR);
       }
       console.info(`Operation success. File created: ${mockFilePath}`);
-      process.exit(ExitCode.SUCCESS);
     });
   }
 };
--- src/service/service.js
+++ src/service/service.js
@@ -1,13 +1,41 @@
 'use strict';
 const {Cli} = require(`./cli`);
-const {DEFAULT_COMMAND, ExitCode} = require(`./const`);
+const {DEFAULT_COMMAND} = require(`./const`);
+const inquirer = require(`inquirer`);
 
-const [, , ...args] = process.argv;
-const [userCommand, count] = args;
+const enterCommand = () => {
+  return inquirer.prompt({
+    name: `userCommand`,
+    type: `list`,
+    message: `Enter command to run:`,
+    choices: [
+      `--version`,
+      `--generate`,
+      `--help`
+    ]
+  });
+};
 
-if (userCommand === `undefined` || !Cli[userCommand]) {
-  Cli[DEFAULT_COMMAND].run();
-  process.exit(ExitCode.success);
-}
+const enterCount = () => {
+  return inquirer.prompt({
+    name: `count`,
+    type: `input`,
+    message: `Enter offers count:`,
+    validate: (count) => parseInt(count, 10) ? true : `Please, enter offers count`
+  });
+};
 
-Cli[userCommand].run(count);
+const run = async () => {
+  const {userCommand} = await enterCommand();
+
+  if (userCommand === `undefined` || !Cli[userCommand]) {
+    Cli[DEFAULT_COMMAND].run();
+  } else if (userCommand === `--generate`) {
+    const {count} = await enterCount();
+    Cli[userCommand].run(count);
+  } else {
+    Cli[userCommand].run();
+  }
+};
+
+run();
