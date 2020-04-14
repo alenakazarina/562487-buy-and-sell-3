f3432f70c1f67d28df7345e0311834b10ebc4563
Alena Kazarina <alenakazarina1@gmail.com>
2020-04-14 17:10:51 +0300

    added api

--- src/service/cli/server.js
+++ src/service/cli/server.js
@@ -1,39 +1,22 @@
 'use strict';
 const express = require(`express`);
-const fs = require(`fs`).promises;
-const {Commands, HttpCode, DataPath, InputData} = require(`../const`);
+const {apiRouter} = require(`./api-router`);
+const {Commands, HttpCode, InputData} = require(`../const`);
+const store = require(`./db/store`);
 
 module.exports = {
   name: Commands.SERVER,
   run: (count) => {
     const port = Number.parseInt(count, 10) || InputData.DEFAULT_PORT;
-
     const server = express();
+    store.init();
+    server.use(express.urlencoded({extended: false}));
     server.use(express.json());
-    server.get(`/offers`, async (req, res) => {
-      try {
-        const fileContent = await fs.readFile(DataPath.OUT);
-        if (fileContent.length) {
-          const mocks = JSON.parse(fileContent);
-          res.json(mocks);
-        } else {
-          res.json([]);
-        }
-      } catch (err) {
-        if (err.code === `ENOENT`) {
-          res.json([]);
-        } else {
-          res
-            .status(HttpCode.INTERNAL_SERVER_ERROR)
-            .send(err);
-        }
-      }
-    });
-
+    server.use(`/api`, apiRouter);
     server.use((req, res) => (
       res
         .status(HttpCode.NOT_FOUND)
-        .send(`Not Found`)
+        .send(`Not found`)
     ));
 
     server.listen(port, () => {
--- src/service/const.js
+++ src/service/const.js
@@ -26,6 +26,9 @@ const Commands = {
 
 const HttpCode = {
   OK: 200,
+  CREATED: 201,
+  NO_CONTENT: 204,
+  BAD_REQUEST: 400,
   UNAUTHORIZED: 401,
   FORBIDDEN: 403,
   NOT_FOUND: 404,
